261ad6daef15007b5a08a2424f07a1b62048e22a
renatoabreu022 <renatoabreu022@gmail.com>
2026-03-30 19:50:08 +0100

    sgdhdhd

--- working/main.py
+++ working/main.py
@@ -6,50 +6,58 @@ from user import Sistema, Utilizador, Perfil
 from percursos import Rua, ParametrosAmbiente, ParametrosPopulacao
 import json
 
+
 def help():
     print("\n--- COMANDOS DISPONÍVEIS ---")
     print("ins_utilizador <nome> <perfil>   -> Regista novo utilizador")
-    #...
-    print("sair                             -> Encerra o programa")
+    print("login <nome> <password>          -> Login")
+    print("gravar <arquivo.json>            -> Salva utilizadores")
+    print("ler <arquivo.json>               -> Carrega utilizadores")
+    print("sair                             -> Encerra a aplicação")
     print("----------------------------")
 
-# --- COMANDOS ---
-
-def cmd_ins_utilizador(args, sist: Sistema):
-    if len(args) < 2:
-        print('Erro: Comando usado indevidamente. Deve seguir o modelo: ins_utilizador <nome> <perfil>')
-        return
-    nome, tipo = args[0], args[1] #assumindo que o username é uma só palavra
-
-    if not sist.cria_username(nome):
-        return
-    
-    perfil = Perfil(tipo)
-    user = Utilizador(nome, '0000', perfil) #aplica uma password temporária
-    sist.add_user(user)
-    print(f'Utilizador "{nome}" com o perfil "{perfil}" criado com sucesso.\n')
-    print('Por padrão, a sua password está definida como "0000". Por favor, mude-a o mais breve possível.')
-
+# --- LOOP PRINCIPAL ---
 
 def main():
-    sist = Sistema() # chamar a base de dados
-
-    print('Seja Bem-vind@! Digite help para esclarecimento de comandos')
+    sistema = Sistema()  # cria a base de dados de utilizadores
+    print("Bem-vind@! Digite 'help' para comandos.")
 
     while True:
-        entrada = input('\n> ').strip().split() #o programa espera até que haja algum input e, caso haja espaços inuteis, remove-os, não limitando tanto a escrita do utilizador
+        entrada = input("> ").strip().split()
         if not entrada:
             continue
 
-        comando = entrada[0].lower() # guarda qual o comando feito
-        args = entrada[1:] # guarda os argumentos do comando usado
+        comando = entrada[0].lower()
+        args = entrada[1:]
 
-        if comando == 'sair':
-            print('A encerrar...')
+        if comando == "sair":
+            print("A encerrar...")
             break
 
-        elif comando == 'help':
+        elif comando == "help":
             help()
+
+        elif comando == "ins_utilizador":
+            if len(args) != 2:
+                print("ERRO: uso indevido do comando.\nDeverá seguir este modelo: ins_utilizador <nome> <perfil>.")
+
+            else:
+                nome, perfil = args
+                u = Utilizador(nome, "1234", Perfil(perfil))  # atribui uma password predifinida, deverá ser trocada pelo utilizador
+                sistema.add_user(u)
+                print(f'Utilizador {nome} criado com o perfil {perfil}.\n Note que lhe foi atribuida a password 0000, por favor, modifique-a.')
         
-        elif comando == 'ins_utilizador':
-            cmd_ins_utilizador(args)
+        elif comando == 'login':
+            if len(args) != 2:
+                print("ERRO: uso indevido do comando.\nDeverá seguir este modelo: login <nome> <password>.")
+            
+            else:
+                nome, password = args
+                if sistema.login(nome,password):
+                    print('Login realizado com sucesso.')
+
+                else:
+                    print('Falha no login.')
+        
+        elif comando == 'gravar':
+            pass
