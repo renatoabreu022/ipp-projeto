246a51405bab317aff61b06195e851615ba94399
Fernando Lucas <fernandolucascosta682@gmail.com>
2026-03-31 15:51:21 +0100

    começamos o IC

--- working/Model_calculopercurso.py
+++ working/Model_calculopercurso.py
@@ -1,1 +1,36 @@
 #------------31/03/2026-------@Lucas@Inês------------------#
+
+#____________imports______________#
+from user.py import Perfil
+from percursos.py import Rua, ParametrosAmbiente
+
+class MotorCalculo:
+
+    @staticmethod
+    def calcular_IC(perfil:Perfil,rua:Rua,ambiente:ParametrosAmbiente): #IC-> Indice de conforto, serve para calcular o conforto do percurso, quanto mais próximo de 1 melhor
+        score = 100
+
+        if 
+
+
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
