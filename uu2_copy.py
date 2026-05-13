c473b596d00dcbe12925055529587d4f7f54ea34
renatoabreu022 <renatoabreu022@gmail.com>
2026-05-13 18:53:52 +0100

    interface definitiva de hoje

--- uu2_copy.py
+++ uu2_copy.py
@@ -13,6 +13,7 @@ from engine.calculo_percurso3 import CalculoPeso
 from models.user import Preferencias
 from models.percursos import ParametrosAcessibilidade, ParametrosAmbiente, ParametrosPopulacao
 from models.grafo import Mapa
+from models.bicicletas import GestorBicicletas
 
 
 ctk.set_appearance_mode("light")
@@ -44,6 +45,225 @@ def carregar_cidades():
 
 # --- BASE DE DADOS DE LOCAIS ---
 DADOS_CIDADES = carregar_cidades()
+class JanelaBicicletas(ctk.CTkToplevel):
+    def __init__(self, master, gestor, db_cidades, mapa, utilizador_nome, bd_utilizadores):
+        super().__init__(master)
+        
+        self.gestor = gestor
+        self.db_cidades = db_cidades
+        self.mapa = mapa
+        self.utilizador_nome = utilizador_nome
+        self.bd_utilizadores = bd_utilizadores
+    
+        self.title("Mobilidade Urbana - Bicicletas")
+        self.geometry("414x740")
+        self.configure(fg_color="#F4F1EA")
+        self.resizable(False, False)
+        
+        self.grab_set() 
+        
+        self.master_app = master # Guarda a referência da janela principal
+        self.gestor = gestor
+        
+        self.cor_fundo = "#F4F1EA"
+        self.cor_verde_forte = "#2A8569"
+        self.cor_texto = "#2A8569"  
+        
+        self.main_container = ctk.CTkFrame(self, fg_color="transparent")
+        self.main_container.pack(fill="both", expand=True, padx=20, pady=20)
+        
+        self.protocol("WM_DELETE_WINDOW", self.voltar_ao_menu_principal)
+        
+        self.mostrar_tela_selecao()
+
+    def limpar_ecra(self):
+        for widget in self.main_container.winfo_children():
+            widget.destroy()
+
+    def mostrar_tela_selecao(self):
+        self.limpar_ecra()
+        
+        ctk.CTkLabel(self.main_container, text="🚲 SISTEMA DE BICICLETAS", 
+                    font=("Helvetica", 24, "bold"), text_color=self.cor_verde_forte).pack(pady=20)
+        
+        #Seleção de Cidade
+        ctk.CTkLabel(self.main_container, text="Selecione a cidade:", font=("Helvetica", 14, "bold"), text_color=self.cor_texto).pack(pady=5)
+        self.combo_cidades = ctk.CTkComboBox(
+            self.main_container, 
+            values=sorted(list(self.db_cidades.keys())), 
+            width=300,
+            command=self.atualizar_origens,
+            state="readonly",
+            fg_color="white",            
+            border_color="#1F634E",     
+            button_color="#1F634E",      
+            button_hover_color="#2A8569", 
+            text_color="black",           
+            dropdown_fg_color="white",     
+            dropdown_text_color="black"    
+        )
+        self.combo_cidades.pack(pady=10)
+        self.combo_cidades.set("Escolha a cidade...")
+
+        #Seleção de Origem
+        ctk.CTkLabel(self.main_container, text="Onde se encontra agora?", font=("Helvetica", 14, "bold"), text_color=self.cor_texto).pack(pady=5)
+        self.combo_origem = ctk.CTkComboBox(
+            self.main_container, 
+            values=[], 
+            width=300,
+            state="readonly",
+            fg_color="white",             
+            border_color="#1F634E",
+            button_color="#1F634E",
+            text_color="black",            
+            dropdown_fg_color="white",
+            dropdown_text_color="black"
+        )
+        self.combo_origem.pack(pady=10)
+        self.combo_origem.set("Selecione a cidade primeiro")
+
+        ctk.CTkButton(self.main_container, text="PESQUISAR ESTAÇÕES", fg_color="#2A8569", hover_color="#1F634E", height=45, width=250, command=self.ir_para_listagem).pack(pady=30)
+        ctk.CTkButton(self.main_container, text="VOLTAR", fg_color="#757575", hover_color="#3E3E3E", command=self.voltar_ao_menu_principal).pack(pady=10)
+        
+    def voltar_ao_menu_principal(self):
+        self.master_app.deiconify() # Mostra a janela principal de volta
+        self.destroy()
+    
+    def atualizar_origens(self, cidade):
+        if hasattr(self, 'cidades_ficheiros') and cidade in self.cidades_ficheiros:
+            ficheiro = self.cidades_ficheiros[cidade]
+            if os.path.exists(ficheiro):
+                self.mapa.load_mapa(ficheiro)
+        
+        locais = self.db_cidades.get(cidade, [])
+        self.combo_origem.configure(values=locais)
+        if locais: self.combo_origem.set(locais[0])
+
+    def ir_para_listagem(self):
+        cidade = self.combo_cidades.get()
+        origem = self.combo_origem.get()
+        if "Escolha" in cidade or not origem:
+            messagebox.showwarning("Aviso", "Selecione a cidade e localização.")
+            return
+        self.mostrar_tela_estacoes(cidade, origem)
+
+    def mostrar_tela_estacoes(self, cidade, origem):
+        self.limpar_ecra()
+        self.gestor.gerar_estacoes_para_cidade(self.db_cidades[cidade])
+        
+        ctk.CTkLabel(self.main_container, text=f"Estações em {cidade}", font=("Helvetica", 20, "bold"), text_color=self.cor_verde_forte).pack(pady=10)
+
+        scroll = ctk.CTkScrollableFrame(self.main_container, fg_color="white", corner_radius=10, height=350)
+        scroll.pack(fill="both", expand=True, padx=10, pady=10)
+
+        for local, info in self.gestor.estacoes.items():
+            f = ctk.CTkFrame(scroll, fg_color="transparent")
+            f.pack(fill="x", pady=5)
+            ctk.CTkLabel(f, text=f"📍 {local}", font=("Helvetica", 12), text_color=self.cor_texto).pack(side="left", padx=10)
+            ctk.CTkLabel(f, text=f"{info['disponiveis']} 🚲 disponíveis", text_color=self.cor_verde_forte, font=("Helvetica", 14, "bold")).pack(side="right", padx=10)
+
+        btn_box = ctk.CTkFrame(self.main_container, fg_color="transparent")
+        btn_box.pack(pady=15)
+        ctk.CTkButton(btn_box, text="CALCULAR PERCURSOS", fg_color="#2A8569", hover_color='#1F4E3D', command=lambda: self.abrir_popup_resultados(origem)).pack(side="left", padx=5)
+        ctk.CTkButton(btn_box, text="VOLTAR", fg_color="#757575", hover_color="#3E3E3E", command=self.mostrar_tela_selecao).pack(side="left", padx=5)
+
+    def abrir_popup_resultados(self, origem):
+        hora_atual = datetime.now().hour
+        ranking = []
+        
+        #preferências reais do utilizador da BD da interface principal
+        prefs_raw = self.bd_utilizadores[self.utilizador_nome]["preferencias"]
+        perfil = Preferencias()
+        perfil.atualizar_parametros(prefs_raw)
+
+        for estacao in self.gestor.estacoes.keys():
+            res = self.mapa.pesquisa_perc(origem, estacao, perfil, hora=hora_atual, k=1)
+            if res:
+                score, caminho = res[0]
+                ranking.append({'local': estacao, 'score': score, 'percurso': caminho})
+
+        if not ranking:
+            messagebox.showerror("Erro", "Não foi possível traçar rotas.")
+            return
+
+        ranking.sort(key=lambda x: x['score'])
+        top_opcoes = ranking[:5]  
+
+        popup = ctk.CTkToplevel(self)
+        popup.title("Melhores Opções Encontradas")
+        popup.geometry("414x740")
+        popup.configure(fg_color=self.cor_fundo)
+        popup.grab_set() 
+
+        popup.protocol("WM_DELETE_WINDOW", lambda: self.fechar_popup(popup))
+    
+        ctk.CTkLabel(popup, text="SUGESTÕES DE PERCURSO", font=("Helvetica", 18, "bold"), text_color=self.cor_verde_forte).pack(pady=15)
+
+        def fechar_popup(self, popup_window):
+            popup_window.destroy()
+            self.deiconify()
+    
+        
+        scroll_res = ctk.CTkScrollableFrame(popup, fg_color="white", corner_radius=12)
+        scroll_res.pack(fill="both", expand=True, padx=20, pady=10)
+
+        for i, opcao in enumerate(top_opcoes):
+            card = ctk.CTkFrame(scroll_res, fg_color="#F9F9F9", corner_radius=8, border_width=1, border_color="#E0E0E0")
+            card.pack(fill="x", pady=8, padx=5)
+
+            
+            header = ctk.CTkFrame(card, fg_color="transparent")
+            header.pack(fill="x", padx=10, pady=5)
+            
+            ctk.CTkLabel(header, text=f"{i+1}ª OPÇÃO: {opcao['local']}", font=("Helvetica", 13, "bold"), text_color=self.cor_verde_forte).pack(side="left")
+            
+            ctk.CTkLabel(header, text=f"Score: {round(opcao['score'], 1)}", font=("Helvetica", 12, "bold"), text_color="#299480").pack(side="right")
+
+            scroll_rota = ctk.CTkScrollableFrame(
+                card, 
+                orientation="horizontal", 
+                fg_color="transparent", 
+                height=60, 
+                scrollbar_button_color=self.cor_verde_forte, 
+                scrollbar_button_hover_color="#2A8569"
+            )
+            scroll_rota.pack(fill="x", padx=10, pady=(0, 5))
+            
+            rota_texto = " -> ".join(opcao['percurso'])
+            lbl_rota = ctk.CTkLabel(
+                scroll_rota, 
+                text=rota_texto, 
+                font=("Helvetica", 11), 
+                text_color=self.cor_texto
+            )
+            lbl_rota.pack(side="left", padx=5)
+
+        ctk.CTkButton(popup, text="FECHAR", fg_color=self.cor_verde_forte, hover_color='#1F4E3D', command=popup.destroy).pack(pady=15)
+        
+    def calcular_rota(self, origem):
+        hora_atual = datetime.now().hour
+        ranking = []
+
+        for estacao in self.gestor.estacoes.keys():
+            res = self.mapa.pesquisa_perc(origem, estacao, self.user_login.u_preferencias, hora=hora_atual, k=1)
+            if res:
+                score, caminho = res[0]
+                ranking.append({'local': estacao, 'score': score, 'percurso': caminho})
+
+        if ranking:
+            ranking.sort(key=lambda x: x['score'])
+            melhor = ranking[0]
+            
+            msg = f"MELHOR OPÇÃO: {melhor['local']}\n"
+            msg += f"Score de Conforto: {round(melhor['score'], 1)}\n\n"
+            msg += f"Caminho: {' -> '.join(melhor['percurso'])}"
+            messagebox.showinfo("Rota Sugerida", msg)
+        else:
+            messagebox.showerror("Erro", "Não foi possível traçar rota para as estações.")
+            
+    def voltar_atras(self):
+        self.pop_listagem.destroy()
+        self.pop_cidade.deiconify()
 
 class AppAcessibilidade(ctk.CTk):
     def __init__(self):
@@ -52,6 +272,8 @@ class AppAcessibilidade(ctk.CTk):
         self.geometry("414x740")
         self.configure(fg_color="#F4F1EA")
         
+        self.motor_mapa = Mapa()
+        self.gestor_bike = GestorBicicletas()
         self.utilizador_atual = None
         self.bd = carregar_bd("utilizadores.json")
         self.mostrar_login()
@@ -87,7 +309,7 @@ class AppAcessibilidade(ctk.CTk):
         self.reg_pass2 = ctk.CTkEntry(self, placeholder_text="Confirmar Password", show="*", width=250)
         self.reg_pass2.pack(pady=10)
         ctk.CTkButton(self, text="Seguinte: Parâmetros", command=self.validar_registo, width=250).pack(pady=30)
-        ctk.CTkButton(self, text="Voltar", command=self.mostrar_login, fg_color="gray").pack()
+        ctk.CTkButton(self, text="Voltar", command=self.mostrar_login, fg_color="#757575", hover_color="#3E3E3E").pack()
 
     def validar_registo(self):
         user, p1, p2 = self.reg_user.get(), self.reg_pass1.get(), self.reg_pass2.get()
@@ -105,7 +327,7 @@ class AppAcessibilidade(ctk.CTk):
         ctk.CTkLabel(self, text="Definições de Perfil", font=("Helvetica", 22, "bold"), text_color="#2A8569").pack(pady=40)
         ctk.CTkButton(self, text="Alterar Parâmetros de Percurso", width=300, command=lambda: self.mostrar_preferencias(False)).pack(pady=15)
         ctk.CTkButton(self, text="Alterar Palavra-passe", width=300, command=self.mostrar_alterar_pass).pack(pady=15)
-        ctk.CTkButton(self, text="Voltar ao Menu Principal", width=300, fg_color="gray", command=self.mostrar_menu_principal).pack(pady=30)
+        ctk.CTkButton(self, text="Voltar ao Menu Principal", width=300, fg_color="#757575", hover_color="#3E3E3E", command=self.mostrar_menu_principal).pack(pady=30)
 
     def mostrar_alterar_pass(self):
         self.limpar_janela()
@@ -115,7 +337,7 @@ class AppAcessibilidade(ctk.CTk):
         self.new_p2 = ctk.CTkEntry(self, placeholder_text="Confirmar Nova Password", show="*", width=250)
         self.new_p2.pack(pady=10)
         ctk.CTkButton(self, text="Confirmar Alteração", command=self.confirmar_mudanca_pass).pack(pady=20)
-        ctk.CTkButton(self, text="Cancelar", fg_color="gray", command=self.mostrar_menu_perfil).pack()
+        ctk.CTkButton(self, text="Cancelar", fg_color="#757575", hover_color="#3E3E3E", command=self.mostrar_menu_perfil).pack()
 
     def confirmar_mudanca_pass(self):
         p1, p2 = self.new_p1.get(), self.new_p2.get()
@@ -225,6 +447,7 @@ class AppAcessibilidade(ctk.CTk):
         f_bottom.pack(fill='x', padx=20, pady=(0,10))
         f_bottom.grid_columnconfigure(0, weight=3)
         f_bottom.grid_columnconfigure(1, weight=1)
+        f_bottom.grid_columnconfigure(2, weight=1)
 
         f_inputs = ctk.CTkFrame(f_bottom, fg_color='transparent')
         f_inputs.grid(row=0, column=0, sticky='ew', padx=(0,10))
@@ -235,12 +458,28 @@ class AppAcessibilidade(ctk.CTk):
         self.cb_destino = ctk.CTkComboBox(f_inputs, values=[], button_color="#2A8569", border_color="#3AC098")
         self.cb_destino.pack(pady=2, fill='x')
 
-        btn_calc = ctk.CTkButton(f_bottom, text='🚶', command=self.simular_calculo, fg_color="#2A8569", hover_color="#1F4E3D", height=65, font=("Helvetica", 13, "bold"))
+        btn_calc = ctk.CTkButton(f_bottom, text='🚶', command=self.simular_calculo, fg_color="#2A8569", hover_color="#1F4E3D", width=60, height=65, font=("Helvetica", 24))
         btn_calc.grid(row=0, column=1, sticky='nsew')
+        btn_bike = ctk.CTkButton(f_bottom, text="🚲", command=self.abrir_interface_bicicletas, fg_color="#2A8569", hover_color="#1F4E3D", width=60, height=65, font=("Helvetica", 24))
+        btn_bike.grid(row=0, column=2, sticky='nsew')
 
-        ctk.CTkButton(self, text="Sair", command=self.mostrar_login, fg_color="#C62828", width=100).pack(pady=5)
+        ctk.CTkButton(self, text="Sair", command=self.mostrar_login, fg_color="#C62828", hover_color="#761616", width=100).pack(pady=5)
 
         self.ao_mudar_cidade(self.cb_cidade.get())
+    def abrir_interface_bicicletas(self):
+        # Dicionário de ficheiros igual ao encontrado no uu2.py
+        cidades_disponiveis = carregar_cidades_disponiveis() 
+        self.withdraw()
+        # Cria a janela de bicicletas (deves copiar a classe JanelaBicicletas do uu2.py para o seu ficheiro ou importá-la)
+        janela = JanelaBicicletas(
+            self, 
+            gestor=self.gestor_bike, 
+            db_cidades=DADOS_CIDADES, 
+            mapa=self.motor_mapa, 
+            utilizador_nome=self.utilizador_atual,
+            bd_utilizadores=self.bd
+        )
+        janela.cidades_ficheiros = cidades_disponiveis
 
     def atualizar_locais(self, cidade):
         locais = DADOS_CIDADES.get(cidade, [])
